394ea447b139411e2e1181f0b6bd959c59a14181
Javier Tuya <tuya@uniovi.es>
2021-11-05 22:33:11 +0100

    Add web test with chrome headless

---
+++ src/it/java/giis/demo/descuento/it/SeleniumUtil.java
@@ -0,0 +1,180 @@
+package giis.demo.descuento.it;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import com.google.common.io.Files;
+
+import giis.demo.util.ApplicationException;
+
+/**
+ * Utilidades varias para uso en los tests con Selenium
+ */
+public class SeleniumUtil {
+	private static final Logger log = LoggerFactory.getLogger("giis.demo.descuento.it.SeleniumUtil");
+	//Archivo de configuracion para las pruebas web, si no existe, las propiedades tomaran valores por defecto
+	private static final String SELENIUM_PROPERTIES = "samples-test-spring.properties";
+	/**
+	 * Instancia un driver para el navegador indicado como parametro.
+	 * WebDriver es un interface que debe instanciarse con el driver correspondiente al navegador a utilizar,
+	 * pero no existe una factoria que lo haga, por lo que se utiliza este metodo.
+	 * 
+	 * <br/>Notar la diferencia entre driver, que es el fichero externo que realiza el interfaz con el navegador
+	 * y los bindings (Selenium Client & WebDriver Language Bindings) que son las dependencias que definen
+	 * los metodos con los que se interactua con WebDriver.
+	 * Tambien existe un remote web driver que permite ejecutar el navegador en otro equipo diferente.
+	 * <br/>Estos drivers son archivos ejecutables que se guardaran en las carpetas de recursos pero que no
+	 * se pueden obtener directamente mediante maven. Para automatizar la obtencion de los drivers
+	 * se utiliza el plugin driver-binary-downloader-maven-plugin (ver pom.xml) que esta configurado
+	 * para que se guarden en una subcarpeta de los recursos denominada selenium-drivers.
+	 * Es muy importante configurar con cuidado las versiones pues muchas veces los nuevos navegadores rompen 
+	 * la compatibilidad con drivers antiguos.
+	 * @param browser indica el navegador utilizado, actualmente implementado: chrome, edge y firefox
+	 * @return el WebDriver instanciado.
+	 */
+	public static WebDriver getNewDriver() {
+		//Utiliza un archivo de propiedades para definir si el driver es local o remoto
+		//En integracion continua con Jenkins se usara el remoto
+		//En integracion continua con GitHub usara un chrome headless que viene instalado en los ejecutores
+		WebDriver driver;
+		String remoteUrl=getRemoteWebDriverUrl();
+		if ("".equals(remoteUrl) || "headless".equals(remoteUrl)) { //driver local
+			//La descarga de binarios con WebDriverManager se suele hacer solamente una vez o una por clase
+			//pero en este caso de ejemplo solo se prueba un escenario, se hace aqui
+			WebDriverManager.chromedriver().setup();
+			log.info("Using local driver");
+			ChromeOptions options = new ChromeOptions();
+			if ("headless".equals(remoteUrl)) {
+				log.info("Using headless driver");
+				options.addArguments("--headless");
+				options.addArguments("--window-size=1024,768");
+			}
+			driver=new ChromeDriver(options);		
+		} else {
+			log.info("Using remote driver: " + remoteUrl);
+			ChromeOptions options=new ChromeOptions();
+			options.setCapability("enableVideo", true);
+			options.setCapability("enableVNC", true);
+			driver=new RemoteWebDriver(getNativeUrl(remoteUrl), options);
+		}
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		return driver;
+	}
+	/**
+	 * Obtiene la url del remote web driver de selenium, si no existe el fichero de configuracion, develve "" (driver local)
+	 */
+	public static String getRemoteWebDriverUrl() {
+		return getProperty(SELENIUM_PROPERTIES, "remote.web.driver.url", "");
+	}
+	/**
+	 * Obtiene la url a probar a partir de la especificada en la configuracion y el puerto indicado como parametro,
+	 * si no existe el fichero de propiedades, utiliza localhost como valor por defecto
+	 */
+	public static String getApplicationUrl(int port) {
+		String url=getProperty(SELENIUM_PROPERTIES, "application.url", "http://localhost") + ":" + port;
+		log.info("Application url: " + url);
+		return url;
+	}
+    private static java.net.URL getNativeUrl(String url) {
+    	try {
+			return new java.net.URL(url);
+		} catch (MalformedURLException e) {
+			throw new ApplicationException("Can't create url "+url);
+		}
+    }
+	public static String getProperty(String propFileName, String propName, String defaultValue) {
+		java.util.Properties prop=new java.util.Properties();
+		File propFile=new File(propFileName);
+		if (!propFile.exists()) {
+			return defaultValue;
+		}
+		try {
+			prop.load(new FileInputStream(propFileName));
+		} catch (IOException e) {
+			throw new ApplicationException("Can't load properties file "+propFileName);
+		}
+		String propValue=prop.getProperty(propName.trim()).trim();
+		if (propValue==null)
+			throw new ApplicationException("Can't read property "+propName);
+		return propValue.trim();
+	}
+
+	/**
+	 * Utilidad para obtencion de todos los elementos de una tabla a partir del WebElement que apunta a esta.
+	 * Obtiene como un string y por orden todos los elementos encerrados entre th o tr.
+	 * Como La tabla puede no tener un numero igual de celdas en cada fila, el numero de columnas devuelto sera
+	 * el de la fila mas larga.
+	 * @param tableElement WebElement que apunta a un elemento table.
+	 * @return Matriz de strings con el contenido de cada celda (incluyendo headers).
+	 */
+	public static String[][] getTableContent(WebElement tableElement) {
+		List<List<String>> orows=new ArrayList<>();
+		int maxcol=0;
+		List<WebElement> irows=tableElement.findElements(By.tagName("tr"));
+		for (WebElement irow : irows) {
+			List<String> ocols=new ArrayList<>();
+			//busca en la fila: elementos td, si no existen busca th
+			List<WebElement> icols=irow.findElements(By.tagName("td"));
+			if (icols.size()==0)
+				icols=irow.findElements(By.tagName("th"));
+			//acumula las celdas de esta fila, calculando siempre el maximo total de columnas
+			int numcol=0;
+			for (WebElement icol : icols) {
+				ocols.add(icol.getText());
+				numcol++;
+				maxcol = maxcol<numcol ? numcol : maxcol;
+			}
+			orows.add(ocols);
+		}
+		//convierte a arrays rellenando con espacios en blanco cuando sea necesario
+		return listToStringMatrix(orows,maxcol);
+	}
+	private static String[][] listToStringMatrix(List<List<String>> irows, int maxcol) {
+		String[][] orows=new String[irows.size()][maxcol];
+		for (int i=0; i<irows.size(); i++) {
+			for (int j=0; j<irows.get(i).size(); j++) //copia columnas de la fila
+				orows[i][j]=irows.get(i).get(j);
+			for (int j=irows.get(i).size(); j<maxcol; j++) //rellena con blancos hasta maxcol
+				orows[i][j]="";
+		}
+		return orows;
+	}
+	/**
+	 * Toma una imagen de la vista actual del navegador y lo guarda en target/screenshots.
+	 * @param name nombre que se dara a la imagen 
+	 * (le anyade un timestamp para diferenciar imagenes guardadas en la misma sesion)
+	 */
+	public static void takeScreenshot(WebDriver driver, String name) {
+		String imageFolderPath = "target/site/";
+		String timestamp = String.valueOf(System.currentTimeMillis()); //diferencia archivos en varias ejecuciones
+		String fileName=imageFolderPath + timestamp + "-" + name + ".png";
+		//toma la imagen y la guarda en el archivo
+		File imageFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			log.info("Take screenshot: " + fileName);
+			File newFile=new File(fileName);
+			Files.createParentDirs(newFile);
+			Files.copy(imageFile, newFile);
+		} catch (IOException e) {
+			throw new ApplicationException(e);
+		}
+	}
+
+}
---
+++ src/it/java/giis/demo/descuento/it/TestDescuentoSelenium.java
@@ -0,0 +1,162 @@
+package giis.demo.descuento.it;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import giis.demo.descuento.*;
+import giis.demo.util.*;
+
+/**
+ * Pruebas de la interaccion del usuario con la aplicacion web del ejemplo de descuentos a clientes 
+ * (Problema 3d)
+ * utilizando Selenium (https://www.seleniumhq.org/):
+ * Ilustra la configuracion para pruebas de integracion con el interfaz web de usuario en Spring Boot
+ * y el proceso basico de ejecucion y comparacion de resultados con Selenium.
+ * <br/>Las situaciones a cubrir son las mismas que en samples-test-java (giis.demo.descuento.it.assertj).
+ * <br/>La configuracion es la siguiente:
+ * 
+ * <br/>SpringBootTest: Al igual que en las pruebas unitarias (TestDescuentoRepository) se reutiliza
+ * la configuracion de la aplicacion, pero se anyade la especificacion del entorno (webEnvironment).
+ * Esto permite que se ejecute la instancia del servidor tomcat embebido con el que se puede interaccionar.
+ * Se especifica RANDOM_PORT para que la prueba no interfiera con otras aplicaciones que puedan estar 
+ * ejecutandose (por ejemplo en CI).
+ * <br/>TestPropertySource: Igual que en las pruebas unitarias (TestDescuentoReposotory)
+ * Especifica un fichero de configuracion diferente (localizado en src/test/resources), 
+ * en este caso solo se omite la carga de datos inicial de data.sql para que se arranque con una base de datos limpia
+ * (esto se podria hacer utilizando un profile diferente con su propia configuracion)
+ * <br/>RunWith: Se ha de especificar el runner especifico para Spring Booth (si no, las anotaciones son ignoradas)
+ * <br/>Notar que no se especifica DataJpaTest pues interfiere con la configuracion especificada de webEnvironment
+ * y falla al intentar lanzar el servidor. 
+ * Si se indica ServletWebServerFactoryAutoConfiguration.class en las clasess de SpringBootTest
+ * El servidor se configura pero incorrectamente sin mapear los endpoints (posible bug?)
+ */
+@SpringBootTest(classes= {DescuentoApplication.class},
+	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations="classpath:application-test.properties")
+//@TestPropertySource(properties = "server.port=8080")
+@RunWith(SpringRunner.class)
+public class TestDescuentoSelenium {
+	//datasource para acceso a la base de datos mediante sql con JdbcTemplate
+	@Autowired private javax.sql.DataSource datasource;
+	//Como se especifico WebEnvironment.RANDOM_PORT, esta variable identifica el puerto utilizado por el servidor
+	@LocalServerPort int port;
+	//Declara el driver de Selenium usado por las pruebas
+	WebDriver driver;
+
+	@Before
+	public void setUp() {
+		loadCleanDatabase();
+		loadMainPage();
+	}
+	@After
+	public void tearDown() {
+		driver.quit(); //cierra el navegador y finaliza la sesion del driver (si se quiere cerrar solo el navegador usar close) 
+	}
+	/**
+	 * Inicializa el WebDriver para el navegador indicado y navega a la pagina principal
+	 */
+	private void loadMainPage() {
+		//Crea una instancia del driver que abrira el navegador
+		driver=SeleniumUtil.getNewDriver();
+		//se dirige a la pagina principal
+		driver.get(SeleniumUtil.getApplicationUrl(port));
+		//ilustra como guardar la imagen del navegador en este momento (el nombre identificara los parametros del paso)
+		SeleniumUtil.takeScreenshot(driver, "main-menu");
+		//selecciona el link para ir a la pagina que se va a probar
+		driver.findElement(By.linkText("Ejecutar descuentos de clientes")).click();
+		SeleniumUtil.takeScreenshot(driver, "main-application");
+	}
+	/**
+	 * Datos de prueba que se cargaran en el setup para cubrir las situaciones del disenyo de la prueba.
+	 * Notar que no se eliminan los datos antes de ejecutar porque el runner establece una transaccion y hace
+	 * rollback al finalizar el test case, asegurando siempre base de datos limpia.
+	 */
+	public void loadCleanDatabase() {
+		//La carga se realiza directqamente sobre la base de datos. 
+		//Esto perimite que los sripts de creacion de datos de prueba sean guardados en fichero externos
+		//cuando se trate de inicializar muchas filas en muchas tablas
+		JdbcTemplate database=new JdbcTemplate(datasource);
+		String sql="insert into cliente(id,edad,nuevo,cupon,tarjeta) values"
+				+"(1,18,'S','N','N'),"
+				+"(2,38,'S','S','N'),"
+				+"(3,21,'S','N','S'),"
+				+"(4,25,'N','N','N'),"
+				+"(5,40,'N','S','N'),"
+				+"(6,42,'N','N','S'),"
+				+"(7,39,'N','S','S')";
+		database.execute(sql);
+	}
+
+	/**
+	 * Escenario de prueba de la pantalla: 
+	 * Cuatro pasos cambiando el filtro por edad, comprobando en cada uno el valor de la tabla de descuentos.
+	 * Como los pasos son similares, encapsula el proceso de ejecucion y comparacion de resultados en 
+	 * un metodo especifico, eliminando codigo duplicado.
+	 */
+	@Test
+	public void testDescuentoScenario() {
+		doStep(true,"","Id,% Descuento\n"
+				+"1,15\n"
+				+"2,20\n"
+				+"5,20\n"
+				+"6,10\n"
+				+"7,30\n");
+		doStep(false, "40", "Id,% Descuento\n"
+				+"5,20\n"
+				+"6,10\n");
+		doStep(false, "39", "Id,% Descuento\n"
+				+"5,20\n"
+				+"6,10\n"
+				+"7,30\n");
+		doStep(false,"","Id,% Descuento\n"
+				+"1,15\n"
+				+"2,20\n"
+				+"5,20\n"
+				+"6,10\n"
+				+"7,30\n");
+	}
+	/**
+	 * La ejecucion de un paso ilustra el uso de las acciones basicas de selenium (findElement, click, sendKeys)
+	 * y otras utilidades (obtener el contenido de una tabla, tomar imagenes)
+	 */
+	private void doStep(boolean initialStep, String edad, String expected) {
+		WebElement txtEdad;
+		//La busqueda de elementos se realiza con findElement, pasando un argumento que indica el criterio.
+		//Por ejmplo, para localizar el campo de texto con la edad:
+		//  txtEdad=driver.findElement(By.id("txtEdad"));
+		//En ocasiones, si no se establecen tiempos de espera entre acciones, es posible que se abra 
+		//la pagina deseada del navegador, pero que todavia no esten presentes los diferentes elementos, 
+		//causando una excepcion. 
+		//Esto es tipico cuando se usa/incluye javascript, y tambien depende del navegador y la velocidad del equipo.
+		//Este elemento se obtendra utilizando una espera explicita
+		txtEdad = (new WebDriverWait(driver, 5))
+			.until(ExpectedConditions.presenceOfElementLocated(By.id("txtEdad")));
+		//Existen tambien formas de establecer un tiempo implicito para el driver.
+		//Ver: https://www.seleniumhq.org/docs/04_webdriver_advanced.jsp#explicit-and-implicit-waits
+		
+		if (initialStep) {
+			assertEquals("",txtEdad.getText()); //asegura que no hay texto
+		} else { //pone la edad y envia el formulario
+			txtEdad.clear(); //si no se limpia antes, sendKeys concatenara con el texto existente
+			txtEdad.sendKeys(edad);
+			driver.findElement(By.id("btnEdad")).click();					
+		}
+		//ilustra como guardar la imagen del navegador en este momento (el nombre identificara los parametros del paso)
+		SeleniumUtil.takeScreenshot(driver, initialStep+"-"+edad);
+		//busca la tabla en el navegador, obtiene el texto de las celdas y la compara como string csv
+		WebElement tab=driver.findElement(By.id("tabDescuentos"));
+		String[][] arrays=SeleniumUtil.getTableContent(tab);
+		assertEquals(expected, Util.arraysToCsv(arrays));
+	}
+
+}
