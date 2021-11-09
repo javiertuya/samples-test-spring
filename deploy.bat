32dc1b736c1f95aff5e29e6ea096588e9e7c6de0
Javier Tuya <tuya@uniovi.es>
2021-11-09 07:36:48 +0100

    Separate production and preproduction deploy

--- deploy.bat
+++ deploy.bat
@@ -3,7 +3,7 @@ rem https://samples-test-spring-develop.herokuapp.com/
 heroku login
 heroku container:login
 mvn clean package -DskipTests=true
-copy target\*-deploy.jar .\samples-test-spring-latest-deploy.jar
+copy target\*-deploy.jar target\samples-test-spring-latest-deploy.jar
 docker build -t samples-test-spring-develop .
 heroku container:push web --app samples-test-spring-develop
 heroku container:release web --app samples-test-spring-develop
