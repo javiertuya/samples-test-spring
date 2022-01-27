e371c0e43fd01b5dd06c57de4aaf9b6a312d75b6
Javier Tuya <tuya@uniovi.es>
2022-01-27 21:57:55 +0100

    Fix breaking change for Spring 2.6.3

--- src/test/java/giis/demo/descuento/ut/TestDescuentoWebController.java
+++ src/test/java/giis/demo/descuento/ut/TestDescuentoWebController.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -38,6 +39,7 @@ import giis.demo.descuento.*;
  * se estaria probando lo mismo dos veces.
  */
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 public class TestDescuentoWebController {
